70b0d6ed886f47769e8e0a8063744445bd6452ed
Andrew Russo and Noah Kittleson <andrew@>
2016-07-05 16:44:07 -0700

    Added JSON Object to the code to get exact data needed.

--- app/src/main/java/com/android/epicodus/stormyapp/MainActivity.java
+++ app/src/main/java/com/android/epicodus/stormyapp/MainActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -18,6 +21,8 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +50,10 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
+                            mCurrentWeather = getCurrentDetails(jsonData);
 
                         } else {
                             alertUserAboutError();
@@ -54,9 +61,13 @@ public class MainActivity extends AppCompatActivity {
                     } catch (IOException e) {
                         Log.e(TAG, "Exception caught: ", e);
                     }
+                    catch (JSONException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+
+                    }
 
                 }
-            });
+             });
         }
         else {
             Toast.makeText(this, R.string.network_unavailable,
@@ -65,6 +76,14 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "Main UI code is running");
 
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: "+ timezone);
+
+        return new CurrentWeather();
     }
 
     private boolean isNetworkAvailable() {
